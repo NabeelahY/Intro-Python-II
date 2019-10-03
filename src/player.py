3981e2c4b0dc47bd1755e62fa0ed9fa8c1f50422
Nabeelah <noblenab13@gmail.com>
2019-10-03 14:43:35 +0100

    Refactor return statement for player class

--- src/player.py
+++ src/player.py
@@ -3,6 +3,11 @@
 
 
 class Player:
-    def __init__(self, room):
+    def __init__(self, name, room):
+        self.name = name
         self.room = room
 
+    def __str__(self):
+        output = ''
+        output += self.room.__str__()
+        return output
